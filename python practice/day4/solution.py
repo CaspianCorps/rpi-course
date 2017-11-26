5f02fe8e89a6c402e10023d663e7ce342783a955
FSZolfaghari <z.fa95@yahoo.com>
2017-11-26 19:31:11 +0330

    solution of problem 4

--- python practice/day4/solution.py
+++ python practice/day4/solution.py
@@ -1,7 +1,29 @@
-import sys
+class Person:
+    def __init__(self, age):
+        if (age < 0):
+            age = 0
+            print("Age is not valid, setting age to 0.")
 
-t = input()
+    def yearPasses(self, age):
+        age += 3
+        return age
 
-print("Hello word")
+    def amIOld(self, age):
+        if age < 13:
+            print("You are young.")
+        elif 13 <= age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
 
-print(t)
+
+T = int(input())
+age = []
+for i in range(T):
+    age.append(int(input()))
+
+for i in range(T):
+    p = Person(age[i])
+    p.amIOld(age[i])
+    p.amIOld(p.yearPasses(age[i]))
+    print("")
